ecff80ad06a7d22c0f3717d0a8f6efef448c2168
DarthDoggy <46265915+DarthDoggy@users.noreply.github.com>
2018-12-30 09:31:05 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,6 +8,14 @@ async def on_ready():
   print(bot.user.name)
   print(bot.user.id)
   
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='/'))
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    """Pings the bot and gets a response time."""
+    pingtime = time.time()
+    pingms = await bot.say("Pinging...")
+    ping = (time.time() - pingtime) * 1000
+    await bot.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
 
 bot.run(os.environ['BOT_TOKEN'])
