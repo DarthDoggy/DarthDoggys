ed5461724ee0f3b9b54b21d2d5ff3dbc40af71f4
DarthDoggy <46265915+DarthDoggy@users.noreply.github.com>
2018-12-30 10:03:47 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,24 +1,38 @@
+
 import discord
 from discord.ext import commands
+import youtube_dl
 import asyncio
 import time
+from itertools import cycle
 import os
 
-@bot.event
+client = commands.Bot(command_prefix=("/"))
+status = ["testing the bot", "/help"]
+
+async def change_status():
+  await client.wait_until_ready()
+  msgs = cycle(status)
+  
+  while not client.is_closed:
+    current_status = next(msgs)
+    await client.change_presence(game=discord.Game(name=current_status))
+    await asyncio.sleep(5)	
+
+@client.event
 async def on_ready():
 	print('Logged in as')
-	print("User name:", bot.user.name)
-	print("User id:", bot.user.id)
+	print("User name:", client.user.name)
+	print("User id:", client.user.id)
 	print('---------------')
-  
-bot = commands.Bot(command_prefix=("d."))
-
+    
 @client.command(pass_context=True)
 async def ping(ctx):
     """Pings the bot and gets a response time."""
     pingtime = time.time()
-    pingms = await bot.say("Pinging...")
+    pingms = await client.say("Pinging...")
     ping = (time.time() - pingtime) * 1000
-    await bot.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
-
-bot.run(os.environ['BOT_TOKEN'])
+    await client.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
+	
+client.loop.create_task(change_status())
+client.run(os.environ['BOT_TOKEN'])
