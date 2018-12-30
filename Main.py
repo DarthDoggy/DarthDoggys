53fba3d57b94ccb8243f85de76b0a08e6ff8bac0
DarthDoggy <46265915+DarthDoggy@users.noreply.github.com>
2018-12-30 09:55:29 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,21 +4,21 @@ import asyncio
 import time
 import os
 
-@client.event
+@bot.event
 async def on_ready():
 	print('Logged in as')
-	print("User name:", client.user.name)
-	print("User id:", client.user.id)
+	print("User name:", bot.user.name)
+	print("User id:", bot.user.id)
 	print('---------------')
   
-client = commands.Bot(command_prefix=("/"))
+bot = commands.Bot(command_prefix=("d."))
 
 @client.command(pass_context=True)
 async def ping(ctx):
     """Pings the bot and gets a response time."""
     pingtime = time.time()
-    pingms = await client.say("Pinging...")
+    pingms = await bot.say("Pinging...")
     ping = (time.time() - pingtime) * 1000
-    await client.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
+    await bot.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
 
-client.run(os.environ['BOT_TOKEN'])
+bot.run(os.environ['BOT_TOKEN'])
