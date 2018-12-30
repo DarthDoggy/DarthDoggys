a56f3fc4bdf0cb18fd82bcba7e0274dcc9b0e863
DarthDoggy <46265915+DarthDoggy@users.noreply.github.com>
2018-12-30 09:41:26 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,21 +3,21 @@ from discord.ext import commands
 import asyncio
 import os
 
-@bot.event
+@client.event
 async def on_ready():
 	print('Logged in as')
 	print("User name:", client.user.name)
 	print("User id:", client.user.id)
 	print('---------------')
   
-bot = commands.Bot(command_prefix=("/"))
+client = commands.Bot(command_prefix=("/"))
 
-@bot.command(pass_context=True)
+@client.command(pass_context=True)
 async def ping(ctx):
     """Pings the bot and gets a response time."""
     pingtime = time.time()
-    pingms = await bot.say("Pinging...")
+    pingms = await client.say("Pinging...")
     ping = (time.time() - pingtime) * 1000
-    await bot.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
+    await client.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
 
-bot.run(os.environ['BOT_TOKEN'])
+client.run(os.environ['BOT_TOKEN'])
