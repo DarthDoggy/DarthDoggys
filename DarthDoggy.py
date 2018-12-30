6d2b995006f322ba0dbb3ba2f813e65418e67144
DarthDoggy <46265915+DarthDoggy@users.noreply.github.com>
2018-12-30 09:20:56 -0800

    Update DarthDoggy.py

--- DarthDoggy.py
+++ DarthDoggy.py
@@ -4,6 +4,10 @@ import os
 
 @bot.event
 async def on_ready():
-print('Logged in as')
-print(bot.user.name)
-print(bot.user.id)
+  print('Logged in as')
+  print(bot.user.name)
+  print(bot.user.id)
+  
+bot = commands.Bot(command_prefix='/')
+
+bot.run(os.environ['BOT_TOKEN'])
