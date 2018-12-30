feab91b490907b18d08d388789c7d45f75b3440f
DarthDoggy <46265915+DarthDoggy@users.noreply.github.com>
2018-12-30 09:35:55 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ async def on_ready():
   print(bot.user.name)
   print(bot.user.id)
   
-bot = commands.Bot(command_prefix=("/"))
+bot = commands.Bot(command_prefix=("d."))
 
 @bot.command(pass_context=True)
 async def ping(ctx):
